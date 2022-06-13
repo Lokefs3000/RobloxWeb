87229dd84c467d7ef6707a4381d722feb08b4270
Lokefs3000 <95932279+Lokefs3000@users.noreply.github.com>
2022-06-13 14:34:29 +0200

    a

---
+++ main.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>How to put PHP in HTML - Simple Example</title>
+        <link href="main.php" rel="php">
+    </head>
+    <body>
+        <p>
+        <?php echo 'Hello world!' ?>
+        </p>
+    </body>
+</html>
